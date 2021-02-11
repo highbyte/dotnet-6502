c68e7ced187e1b83c9843d4b8a34f901fb2a1712
highbyte <highbyte@outlook.com>
2021-02-11 16:56:32 +0100

    Simplify InstructionExecutor addressing mode handling.

--- Highbyte.DotNet6502/InstructionExecutor.cs
+++ Highbyte.DotNet6502/InstructionExecutor.cs
@@ -11,6 +11,13 @@ namespace Highbyte.DotNet6502
         {
         }
 
+        /// <summary>
+        /// Reads instruction from current PC and executes it.
+        /// When method returns, PC will be increased to point at next instruction 
+        /// Returns true if instruction was handled, false is instruction is unknown.
+        /// </summary>
+        /// <param name="cpu"></param>
+        /// <param name="mem"></param>
         public void Execute(CPU cpu, Memory mem)
         {
             byte opCode = cpu.FetchInstruction(mem);
@@ -18,7 +25,9 @@ namespace Highbyte.DotNet6502
         }
 
         /// <summary>
-        /// Executes an instruction.
+        /// Executes the specified instruction.
+        /// PC is assumed to point at the instruction operand, or the the next instruction, depending on instruction.
+        /// When method returns, PC will be increased to point at next instruction 
         /// Returns true if instruction was handled, false is instruction is unknown.
         /// </summary>
         /// <param name="cpu"></param>
@@ -34,168 +43,87 @@ namespace Highbyte.DotNet6502
             if(instruction == null)
                 return false;
 
-            AddrModeCalcResult addrModeCalcResult;
+            AddrModeCalcResult addrModeCalcResult = new AddrModeCalcResult(opCodeObject);
             switch(opCodeObject.AddressingMode)
             {
                 case AddrMode.I:
-                    addrModeCalcResult = GetImmediateModeValue(cpu, mem);
+                {
+                    addrModeCalcResult.InsValue = cpu.FetchOperand(mem);
                     break;
+                }
                 case AddrMode.ZP:
-                    addrModeCalcResult = GetZeroPageModeAddress(cpu, mem);
+                {
+                    addrModeCalcResult.InsAddress = cpu.FetchOperand(mem);
                     break;
+                }
                 case AddrMode.ZP_X:
-                    addrModeCalcResult = GetZeroPageXModeAddress(cpu, mem);
+                {
+                    addrModeCalcResult.InsAddress = cpu.CalcZeroPageAddressX(cpu.FetchOperand(mem), wrapZeroPage: true);
                     break;
+                }
                 case AddrMode.ZP_Y:
-                    addrModeCalcResult = GetZeroPageYModeAddress(cpu, mem);
+                {
+                    addrModeCalcResult.InsAddress = cpu.CalcZeroPageAddressY(cpu.FetchOperand(mem), wrapZeroPage: true);
                     break;
+                }
                 case AddrMode.ABS:
-                    addrModeCalcResult = GetAbsModeAddress(cpu, mem);
+                {
+                    addrModeCalcResult.InsAddress = cpu.FetchOperandWord(mem);
                     break;
+                }
                 case AddrMode.ABS_X:
-                    addrModeCalcResult = GetAbsXModeAddress(cpu, mem);
+                {
+                    // Note: CalcFullAddressX will check if adding X to address will cross page boundary. If so, one more cycle is consumed.
+                    addrModeCalcResult.InsAddress = cpu.CalcFullAddressX(cpu.FetchOperandWord(mem), out bool didCrossPageBoundary, false);
+                    addrModeCalcResult.AddressCalculationCrossedPageBoundary = didCrossPageBoundary;
                     break;
+                }
                 case AddrMode.ABS_Y:
-                    addrModeCalcResult = GetAbsYModeAddress(cpu, mem);
+                {
+                    // Note: CalcFullAddressY will check if adding Y to address will cross page boundary. If so, one more cycle is consumed.
+                    addrModeCalcResult.InsAddress = cpu.CalcFullAddressY(cpu.FetchOperandWord(mem), out bool didCrossPageBoundary, false);
+                    addrModeCalcResult.AddressCalculationCrossedPageBoundary = didCrossPageBoundary;
                     break;
+                }
                 case AddrMode.IX_IND:
-                    addrModeCalcResult = GetIndexedIndirectModeAddress(cpu, mem);
-                    break;  
+                {
+                    addrModeCalcResult.InsAddress = cpu.FetchWord(mem, cpu.CalcZeroPageAddressX(cpu.FetchOperand(mem)));
+                    break;
+                }
                 case AddrMode.IND_IX:
-                    addrModeCalcResult = GetIndirectIndexedModeAddress(cpu, mem);
+                {
+                    addrModeCalcResult.InsAddress = cpu.CalcFullAddressY(cpu.FetchWord(mem, cpu.FetchOperand(mem)), out bool didCrossPageBoundary, false);
+                    addrModeCalcResult.AddressCalculationCrossedPageBoundary = didCrossPageBoundary;
                     break;
+                }
                 case AddrMode.Indirect:
-                    addrModeCalcResult = GetIndirectModeAddress(cpu, mem);
+                {
+                    addrModeCalcResult.InsAddress = cpu.FetchWord(mem, cpu.FetchOperandWord(mem));
                     break;
+                }
                 case AddrMode.Relative:
-                    addrModeCalcResult = GetRelativeModeAddress(cpu, mem);
+                {
+                    addrModeCalcResult.InsValue = cpu.FetchOperand(mem);
                     break;
+                }
                 case AddrMode.Accumulator:
+                {
                     // This mode has no value or address
-                    addrModeCalcResult = new AddrModeCalcResult();
                     break;
+                }
                 case AddrMode.Implied:
+                {
                     // This mode has no value or address
-                    addrModeCalcResult = new AddrModeCalcResult();
                     break;
-
+                }
                  default:
                     return false;
             }
+
             if(addrModeCalcResult==null)
                 throw new DotNet6502Exception("Bug detected. Variable addrModeCalcResult expected to be set.");
 
-            // TODO: Ugly setting OpCode here
-            addrModeCalcResult.OpCode = opCodeObject; 
             return instruction.Execute(cpu, mem, addrModeCalcResult);
         }
-
-        public AddrModeCalcResult GetImmediateModeValue(CPU cpu, Memory mem)
-        {
-            return new AddrModeCalcResult
-            {
-                InsValue = cpu.FetchOperand(mem)
-            };
-        }
-        public AddrModeCalcResult GetZeroPageModeAddress(CPU cpu, Memory mem)
-        {
-            return new AddrModeCalcResult
-            {
-                InsAddress = cpu.FetchOperand(mem)
-            };
-        }
-
-        public AddrModeCalcResult GetZeroPageXModeAddress(CPU cpu, Memory mem)
-        {
-            var operandAddress = cpu.FetchOperand(mem);
-            return new AddrModeCalcResult
-            {
-                InsAddress = cpu.CalcZeroPageAddressX(operandAddress, wrapZeroPage: true)
-            };
-        }
-
-        public AddrModeCalcResult GetZeroPageYModeAddress(CPU cpu, Memory mem)
-        {
-            var operandAddress = cpu.FetchOperand(mem);
-            return new AddrModeCalcResult
-            {
-                InsAddress = cpu.CalcZeroPageAddressY(operandAddress, wrapZeroPage: true)
-            };
-        }
-
-        public AddrModeCalcResult GetAbsModeAddress(CPU cpu, Memory mem)
-        {
-            return new AddrModeCalcResult
-            {
-                InsAddress = cpu.FetchOperandWord(mem)
-            };
-        }
-
-        public AddrModeCalcResult GetAbsXModeAddress(CPU cpu, Memory mem)
-        {
-            var operandAddress = cpu.FetchOperandWord(mem);
-            // Note: CalcFullAddressX will check if adding X to address will cross page boundary. If so, one more cycle is consumed.
-            var insAddress = cpu.CalcFullAddressX(operandAddress, out bool didCrossPageBoundary, false);
-            return new AddrModeCalcResult
-            {
-                InsAddress = insAddress,
-                AddressCalculationCrossedPageBoundary = didCrossPageBoundary
-            };
-        }
-
-        public AddrModeCalcResult GetAbsYModeAddress(CPU cpu, Memory mem)
-        {
-            var operandAddress = cpu.FetchOperandWord(mem);
-            // Note: CalcFullAddressY will check if adding Y to address will cross page boundary. If so, one more cycle is consumed.
-            var insAddress = cpu.CalcFullAddressY(operandAddress, out bool didCrossPageBoundary, false);
-            return new AddrModeCalcResult
-            {
-                InsAddress = insAddress,
-                AddressCalculationCrossedPageBoundary = didCrossPageBoundary                
-            };
-        }
-
-        public AddrModeCalcResult GetIndexedIndirectModeAddress(CPU cpu, Memory mem)
-        {
-            var operandAddress = cpu.FetchOperand(mem);
-            var zeroPageAddressX = cpu.CalcZeroPageAddressX(operandAddress);
-            return new AddrModeCalcResult
-            {
-                InsAddress = cpu.FetchWord(mem, zeroPageAddressX)
-            };
-        }
-
-        public AddrModeCalcResult GetIndirectIndexedModeAddress(CPU cpu, Memory mem)
-        {
-            var operandAddress = cpu.FetchOperand(mem);
-            var indirectIndexedAddress = cpu.FetchWord(mem, operandAddress);
-            // Note: CalcFullAddressY will check if adding Y to address will cross page boundary. If so, one more cycle is consumed.
-            var insAddress = cpu.CalcFullAddressY(indirectIndexedAddress, out bool didCrossPageBoundary, false);
-            return new AddrModeCalcResult
-            {
-                InsAddress = insAddress,
-                AddressCalculationCrossedPageBoundary = didCrossPageBoundary
-            };
-        }
-
-        public AddrModeCalcResult GetIndirectModeAddress(CPU cpu, Memory mem)
-        {
-            ushort operandAddress = cpu.FetchOperandWord(mem);
-            // Get actual address from the operandAddress location
-            return new AddrModeCalcResult
-            {
-                InsAddress = cpu.FetchWord(mem, operandAddress)
-            };
-        }
-
-       public AddrModeCalcResult GetRelativeModeAddress(CPU cpu, Memory mem)
-        {
-            // The operand (signed byte) contains a relative address (positive or negative)
-            return new AddrModeCalcResult
-            {
-                InsValue = cpu.FetchOperand(mem)
-            };
-        }
-
     }
 }
