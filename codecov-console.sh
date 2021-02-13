dd7eaac1d161c717adacb18b42e91e43ebbf9cde
highbyte <highbyte@outlook.com>
2021-02-13 13:06:46 +0100

    Fix Linux line-endings ins codecov-console.sh script

--- codecov-console.sh
+++ codecov-console.sh
@@ -5,3 +5,4 @@ rm -rf ./Highbyte.DotNet6502.Tests/TestResults
 dotnet test --filter TestType!=Integration --collect:"XPlat Code Coverage"
 reportgenerator -reports:./**/TestResults/**/coverage.cobertura.xml -targetdir:coveragereport -reporttypes:TextSummary
 cat ./coveragereport/Summary.txt
+
