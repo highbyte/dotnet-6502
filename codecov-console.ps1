5c236c2a5e464e31cbecee42f65fa44cd5e51389
highbyte <highbyte@outlook.com>
2021-02-13 16:33:43 +0100

    Fix unit test scripts to only compile and run test project.

--- codecov-console.ps1
+++ codecov-console.ps1
@@ -1,5 +1,5 @@
 rm ./coveragereport -Force -Recurse -ErrorAction SilentlyContinue
 rm ./Highbyte.DotNet6502.Tests/TestResults -Force -Recurse -ErrorAction SilentlyContinue
-dotnet test --filter TestType!=Integration --collect:"XPlat Code Coverage"
+dotnet test Highbyte.DotNet6502.Tests --filter TestType!=Integration --collect:"XPlat Code Coverage"
 reportgenerator -reports:./**/TestResults/**/coverage.cobertura.xml -targetdir:coveragereport -reporttypes:TextSummary
 cat ./coveragereport/Summary.txt
--- codecov-console.sh
+++ codecov-console.sh
@@ -2,7 +2,7 @@
 
 rm -rf ./coveragereport
 rm -rf ./Highbyte.DotNet6502.Tests/TestResults
-dotnet test --filter TestType!=Integration --collect:"XPlat Code Coverage"
+dotnet test Highbyte.DotNet6502.Tests --filter TestType!=Integration --collect:"XPlat Code Coverage"
 reportgenerator -reports:./**/TestResults/**/coverage.cobertura.xml -targetdir:coveragereport -reporttypes:TextSummary
 cat ./coveragereport/Summary.txt
 
