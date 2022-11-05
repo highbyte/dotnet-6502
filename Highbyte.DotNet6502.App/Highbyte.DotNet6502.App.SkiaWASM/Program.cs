b964069b014d9907f6c50a2956760f642c3503fd
Highbyte <highbyte@outlook.com>
2022-11-05 22:13:24 +0100

    Upgrade to .NET 7 rc2 to fix AOT runtime issue

--- Highbyte.DotNet6502.App/Highbyte.DotNet6502.App.SkiaWASM/Program.cs
+++ Highbyte.DotNet6502.App/Highbyte.DotNet6502.App.SkiaWASM/Program.cs
@@ -1,23 +1,12 @@
 using Blazored.Modal;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Highbyte.DotNet6502.App.SkiaWASM;
 
-namespace Highbyte.DotNet6502.App.SkiaWASM
-{
-    public class Program
-    {
-        public static async Task Main(string[] args)
-        {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("#app");
-            builder.RootComponents.Add<HeadOutlet>("head::after");
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddBlazoredModal();
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-            builder.Services.AddBlazoredModal();
-
-            var host = builder.Build();
-
-            await host.RunAsync();
-        }
-    }
-}
+await builder.Build().RunAsync();
