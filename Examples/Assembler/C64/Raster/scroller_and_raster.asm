a11831122168f3f2275082f82d77dede1e9d6742
Highbyte <highbyte@outlook.com>
2023-07-24 17:14:56 +0200

    Improve C64 raster+scroll example

--- Examples/Assembler/C64/Raster/scroller_and_raster.asm
+++ Examples/Assembler/C64/Raster/scroller_and_raster.asm
@@ -7,11 +7,35 @@
 ;------------------------------------------------------------
 ;Program settings
 ;------------------------------------------------------------
-STATIC_TEXT_ROW = 8;
-COLOR_CYCLE_EACH_X_FRAME = 2;
+STATIC_TEXT_ROW = 9
+COLOR_CYCLE_EACH_X_FRAME = 2
 
-SCROLLER_ROW = 14;
-SCROLL_EACH_X_FRAME = 4;
+SCROLLER_ROW = 15
+SCROLL_EACH_X_FRAME = 4
+
+RASTER_DEFAULT_BORDER_COLOR = 0x00
+RASTER_DEFAULT_BACKGROUND_COLOR = 0x00
+
+RASTER_BACKGROUND_DISABLED_START = 51 + (7*8) 	;First raster line of main screen, NTSC & PAL = 51
+
+RASTER_BACKGROUND_DISABLED_END = 251 - 8 - (7*8)	;Last raster line of main screen, NTSC & PAL = 250.
+
+ColorBlack = 0x00
+ColorWhite = 0x01
+ColorRed = 0x02
+ColorCyan = 0x03
+ColorViolet = 0x04
+ColorGreen = 0x05
+ColorBlue = 0x06
+ColorYellow = 0x07
+ColorOrange = 0x08
+ColorBrown = 0x09
+ColorLightRed = 0x0a
+ColorDarkGrey = 0x0b
+ColorGrey = 0x0c
+ColorLightGreen = 0x0d
+ColorLightBlue = 0x0e
+ColorLightGrey = 0x0f
 
 ;------------------------------------------------------------
 ;Memory address shared with emulator host for updating screen
@@ -49,30 +73,43 @@ ZP_SCROLL_TEXT_ADDRESS = 0xfb
 ;Index to where in text color list
 ZP_TEXT_COLOR_CYCLE_INDEX = 0xfd
 
-;Index to where in background color list 
-ZP_BG_COLOR_CYCLE_INDEX = 0xfe
-
-;Index to where in border color list
-ZP_BORDER_COLOR_CYCLE_INDEX = 0x02
-
 ;Frame counters
-ZP_SCROLL_FRAME_COUNT = 0x03
-ZP_COLOR_CYCLE_FRAME_COUNT = 0x04
+ZP_SCROLL_FRAME_COUNT = 0xfe
+ZP_COLOR_CYCLE_FRAME_COUNT = 0x02
 
 ;Index in raster sine table
-RASTER_COUNTER = 0x05
+RASTER_COUNTER = 0x03
+
+CURRENT_RASTER_LINE = 0x04
+PREVIOUS_RASTER_LINE = 0x05
+RASTER_BAR_DIRECTION = 0x06	;Down = 0, Up = 1
 
 ;Macros
-!macro wait_vblank {
-;Wait for vblank (if raster pos 255 already been waited for, set X to no-zero)
-	cpx #0
-	bne .wait_vblank2
-.wait_vblank1
-	bit SCREEN_CONTROL_REGISTER_1
-	bpl .wait_vblank1
-.wait_vblank2
-	bit SCREEN_CONTROL_REGISTER_1
-	bmi .wait_vblank2
+!macro set_irq .irqhandler, .line {
+
+	; Set next IRQ raster line
+	lda #<.line ; Bits 0-7 of current raster line
+	sta SCREEN_RASTER_LINE	; $d012
+	lda #>.line ; 8th bit of current raster line
+	cmp #0
+	beq .no_highbit
+	;Set bit 7 of $d011, which is the 8th bit of the current raster line
+	lda SCREEN_CONTROL_REGISTER_1 ; $d011
+	ora #128
+	sta SCREEN_CONTROL_REGISTER_1 ; $d011
+	jmp .irq_addr_cont
+.no_highbit:
+	;Clear bit 7 of $d011, which is the 8th bit of the current raster line
+	lda SCREEN_CONTROL_REGISTER_1 ; $d011
+	and #127
+	sta SCREEN_CONTROL_REGISTER_1 ; $d011
+.irq_addr_cont:
+
+	; The handler that will be called during the IRQ
+	lda #<.irqhandler
+	sta $0314
+	lda #>.irqhandler
+	sta $0315
 }
 
 ;------------------------------------------------------------------
@@ -80,24 +117,12 @@ RASTER_COUNTER = 0x05
 ;------------------------------------------------------------------
 	sei			;Disable interrupts;
 
-;Set default border and background color
-	lda #0x0b
-	sta defaultBorderColor
-	lda #0x00
-	sta defaultBackgroundColor
-
-;Init bg color cycle index
-	lda #2
-	sta ZP_BG_COLOR_CYCLE_INDEX
-
-;Init border color cycle index
-	lda #0
-	sta ZP_BORDER_COLOR_CYCLE_INDEX
-
 ;Clear text screen and set text color
 	jsr cleartextscreen
 	lda #1	;text color
-	jsr settextcolors
+	jsr setalltextcolors
+
+	;jsr setcustomcharactersandcolors
 
 ;Initialize scroll text address to start of text.
 	jsr initscroll
@@ -108,17 +133,21 @@ RASTER_COUNTER = 0x05
 	jsr initcolorcycleframecount
 	jsr initcolorcycle
 
+; Init IRQ and first IRQ handler
+	jsr initirq
+
+; Clear interrupt flag, allowing the CPU to respond to interrupt requests
+	cli                  
+
 !zone mainloop
 mainloop:
-;Wait for new frame (flag set by emulator host)
-	ldx #0
-	+wait_vblank
+	;Raster bars
+	jsr rasterbars
 
-;Cycle background color if key is pressed
-;	jsr cyclebackgroundifkeyispressed
-;Cycle border color if key is pressed
-;	jsr cycleborderifkeyispressed
+	jmp mainloop
 
+;------------------------------------------------------------------
+vblank_irq:
 ;Color cycle (evry frame)
 	;Check how often we should scroll (every x frame)
  	dec ZP_COLOR_CYCLE_FRAME_COUNT
@@ -133,16 +162,11 @@ skipcolorcycle:
  	bne skipscroll
  	jsr scrolltext
  	jsr initscrollframecount
-
 skipscroll:
 
-;Raster bars
-	jsr rasterbars
-
-	;brk	; In emulator, setup hitting brk instruction to stop	
-	jmp mainloop
-	;brk	; In emulator, setup hitting brk instruction to stop
-;------------------------------------------------------------------
+	ASL $D019            ; acknowledge the interrupt by clearing the VIC's interrupt flag
+	;JMP $EA31            ; jump into KERNAL's standard interrupt service routine to handle keyboard scan, cursor display etc.	
+	JMP $EA81            ; jump into shorter ROM routine to only restore registers from the stack etc
 
 !zone cleartextscreen
 cleartextscreen:
@@ -157,9 +181,9 @@ cleartextscreen:
 	bne .loop
 	rts
 
-!zone settextcolors
+!zone setalltextcolors
 ;Set color for each character on screen to the value in A
-settextcolors:
+setalltextcolors:
 	ldx #0
 .loop:
 	sta SCREEN_COLOR_MEM,x
@@ -169,7 +193,6 @@ settextcolors:
 	dex
 	bne .loop
 	rts
-
 
 !zone printstatictext
 printstatictext:
@@ -274,41 +297,100 @@ initscrollframecount:
 rasterbars:
 	ldx RASTER_COUNTER
 	lda rasterSinusTable,x	;Grab new rasterline value  
+	lsr 					;Divide by 2 to get smaller sine wave
+	clc
+	adc #66  			    ;Adjust to middle of screen
 .rasterwait:
 	cmp SCREEN_RASTER_LINE	;from the table and wait
-    bne .rasterwait		;for raster the line
+    bne .rasterwait			;for raster the line
 
-	ldy #10				;Loose time to hide the
-.idle1	
-	dey					;flickering at the beginning 
-	bne .idle1			;of the effect
+	ldy #10					;Loose time to hide the
+.idle1		
+	dey						;flickering at the beginning 
+	bne .idle1				;of the effect
 
+
+ 	sta CURRENT_RASTER_LINE	;Save current rasterbars start position
+	cmp PREVIOUS_RASTER_LINE	;Compare with previous rasterline start position
+	sta PREVIOUS_RASTER_LINE	;Remember the current raster start as previous
+	bcc .goingdown 			;Previous less than -> going down
+	lda #1					;1 = going up
+	sta RASTER_BAR_DIRECTION
+	jmp .afterdirection
+.goingdown
+	lda #0					;0 = going down
+	sta RASTER_BAR_DIRECTION
+.afterdirection
 ; Main Loop to print raster bars
-	ldx #00		
-.loop	
+	ldx #00
+.loop
+  	lda CURRENT_RASTER_LINE
+ 	cmp #RASTER_BACKGROUND_DISABLED_START
+ 	bcc .backgroundAndBorderRaster	;Less than
+ 	cmp #RASTER_BACKGROUND_DISABLED_END
+ 	bcs .backgroundAndBorderRaster	;Greater than or equal
+
+;Within main defined area where raster in background should be on or off (depending on direction check above)
+	lda RASTER_BAR_DIRECTION
+	cmp #0
+	beq .backgroundAndBorderRaster	;If going down, show both background and border raster
+	;Only show border raster, and background to same color as default background
+	lda rasterColorTable,x	;assign border
+   	sta SCREEN_BORDER_COLOR_ADDRESS
+	lda #RASTER_DEFAULT_BACKGROUND_COLOR 	;assign border to default background color
+	sta SCREEN_BACKGROUND_COLOR_ADDRESS
+	jmp .cont
+
+.backgroundAndBorderRaster
 	lda rasterColorTable,x	;assign background and border
    	sta SCREEN_BORDER_COLOR_ADDRESS
 	sta SCREEN_BACKGROUND_COLOR_ADDRESS
+.cont
 
-	ldy rasterDelayTable,x	;Loose time to hide the
-.idle2	
-	dey					;flickering at the end
-	bne .idle2			;of the effect
+; 	ldy rasterDelayTable,x	;Loose time to hide the
+; .idle2	
+; 	dey						;flickering at the end
+; 	bne .idle2				;of the effect
 
+	inc CURRENT_RASTER_LINE
+	inc CURRENT_RASTER_LINE
+	lda CURRENT_RASTER_LINE
+.waitnextline:
+	cmp SCREEN_RASTER_LINE
+	bne .waitnextline
 
 	inx 		
-	cpx #09
+	cpx #rasterLength
 	bne .loop
 ; End of main loop
-
-	;Assign default border and background colors
-	lda defaultBorderColor			
-	sta SCREEN_BORDER_COLOR_ADDRESS
-	lda defaultBackgroundColor
-	sta SCREEN_BACKGROUND_COLOR_ADDRESS
 	
 	inc RASTER_COUNTER
 
+	;Raster bar done, reset border and background color
+	lda #RASTER_DEFAULT_BORDER_COLOR
+	sta SCREEN_BORDER_COLOR_ADDRESS
+	lda #RASTER_DEFAULT_BACKGROUND_COLOR
+	sta SCREEN_BACKGROUND_COLOR_ADDRESS
+
+	rts
+
+;------------------------------------------------------------
+!zone initirq
+initirq:
+	LDA #%01111111       ; switch off interrupt signals from CIA-1
+	STA $DC0D
+
+	AND $D011            ; clear most significant bit of VIC's raster register
+	STA $D011
+
+	LDA $DC0D            ; acknowledge pending interrupts from CIA-1
+	LDA $DD0D            ; acknowledge pending interrupts from CIA-2
+
+	; Setup first IRQ to raster line 0
+	+set_irq vblank_irq, 0
+
+	LDA #%00000001       ; enable raster interrupt signals from VIC
+	STA $D01A
 	rts
 
 ;------------------------------------------------------------
@@ -319,7 +401,7 @@ rasterbars:
 !convtab scr	;Text conversion setting: pet (PetSCII), raw (none), scr (C64 screen code)
 
 STATIC_TEXT:
-	!text "C64 code running in dotnet6502 emulator"
+	!text "c-64 code running in dotnet6502 emulator"
 	!by 0 ;End of text indicator
 
 STATIC_TEXT_COLOR:
@@ -334,22 +416,21 @@ SCROLL_TEXT:
 	;!text "                                                                                "
 	!text "                                        "
 	!text "highbyte, in 2023, proudly presents... a dotnet 6502 cpu emulator!    "
-	!text "these raster bars and the (rather choppy) scroller is written in 6502 machine code for C64.   "
+	!text "these raster bars and the (rather choppy) scroller is written in 6502 machine code for c64.   "
 	;!text "hold space to flash border color.   "
 	!text "greetings to all my demo-scene friends from back in the late 80s & early 90s in the groups them and virtual!"
 	;!text "                                                                                "
 	!text "                                        "
 	!by 0 ;End of text indicator
 
-
-defaultBorderColor:
-	!by 0x0b
-
-defaultBackgroundColor:
-	!by 0x00
+rasterLength = 9;
 
 rasterColorTable:
-!by 09,08,12,13,01,13,12,08,09
+;!by 09,08,12,13,01,13,12,08,09
+;!by ColorDarkGrey, ColorBlue, ColorLightBlue, ColorLightGrey, ColorWhite, ColorLightGrey, ColorLightBlue, ColorBlue, ColorDarkGrey
+;!by ColorDarkGrey, ColorGreen, ColorLightGreen, ColorLightGrey, ColorWhite, ColorLightGrey, ColorLightGreen, ColorGreen, ColorDarkGrey
+;!by ColorDarkGrey, ColorRed, ColorLightRed, ColorLightGrey, ColorWhite, ColorLightGrey, ColorLightRed, ColorRed, ColorDarkGrey
+!by ColorBrown, ColorRed, ColorLightRed, ColorOrange, ColorYellow, ColorOrange, ColorLightRed, ColorRed, ColorBrown
 
 rasterDelayTable:
 !by 08,08,09,08,12,08,08,08,09
