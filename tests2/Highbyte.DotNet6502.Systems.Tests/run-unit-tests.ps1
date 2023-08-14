90bcd223c98ffe68f1a2b8f9e64b836adf8a5691
Christer C <highbyte@outlook.com>
2023-08-14 23:18:07 +0200

    Delete tests2 directory

--- tests2/Highbyte.DotNet6502.Systems.Tests/run-unit-tests.ps1
+++
@@ -1,2 +0,0 @@
-#Based on xUnit Trait attribute on tests
-dotnet test --filter TestType!=Integration
--- tests2/Highbyte.DotNet6502.Tests/GlobalUsings.cs
+++
@@ -1,5 +0,0 @@
-// If <ImplicitUsings>enable</ImplicitUsings> is added to the .csproj file, a bunch of standard .NET namespaces are added by default (and not needed to be added here).
-
-global using Xunit;
-global using Xunit.Abstractions;
-global using Highbyte.DotNet6502.Tests.Helpers;
