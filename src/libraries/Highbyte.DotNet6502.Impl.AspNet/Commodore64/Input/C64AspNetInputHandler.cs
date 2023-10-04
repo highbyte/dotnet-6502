df1a3c3f98230274a581a59a9bbd5550fdbfe51e
Highbyte <highbyte@outlook.com>
2023-10-04 23:33:38 +0200

    C64 joystick keyboard in WASM input handler.

--- src/libraries/Highbyte.DotNet6502.Impl.AspNet/Commodore64/Input/C64AspNetInputHandler.cs
+++ src/libraries/Highbyte.DotNet6502.Impl.AspNet/Commodore64/Input/C64AspNetInputHandler.cs
@@ -1,7 +1,9 @@
+using Highbyte.DotNet6502.Instructions;
 using Highbyte.DotNet6502.Systems;
 using Highbyte.DotNet6502.Systems.Commodore64;
 using Highbyte.DotNet6502.Systems.Commodore64.TimerAndPeripheral;
 using Highbyte.DotNet6502.Systems.Commodore64.Video;
+using static Highbyte.DotNet6502.Systems.Commodore64.TimerAndPeripheral.C64Joystick;
 
 namespace Highbyte.DotNet6502.Impl.AspNet.Commodore64.Input;
 
@@ -27,6 +29,8 @@ public class C64AspNetInputHandler : IInputHandler<C64, AspNetInputHandlerContex
     public void ProcessInput(C64 c64)
     {
         CaptureKeyboard(c64);
+
+        CaptureJoystick(c64);
 
         _inputHandlerContext!.ClearKeys();   // Clear our captured keys so far
     }
@@ -113,17 +117,7 @@ public class C64AspNetInputHandler : IInputHandler<C64, AspNetInputHandlerContex
 
         foreach (var key in _inputHandlerContext.KeysPressed)
         {
-            char character;
-            if (key.Length == 1)
-            {
-                character = key[0];
-            }
-            else
-            {
-                character = MapAspNetKeyStringToCharacter(key);
-                if (character == 0)
-                    continue;
-            }
+            var character = MapAspNetKeyStringToCharacter(key);
             if (!Petscii.CharToPetscii.ContainsKey(character))
                 continue;
             var petsciiCode = Petscii.CharToPetscii[character];
@@ -133,9 +127,43 @@ public class C64AspNetInputHandler : IInputHandler<C64, AspNetInputHandlerContex
 
     private char MapAspNetKeyStringToCharacter(string key)
     {
+        char character;
+        if (key.Length == 1)
+            return key[0];
+
+        character = (char)0;
         if (C64AspNetKeyboard.AspNetKeyStringToCharacter.ContainsKey(key))
-            return C64AspNetKeyboard.AspNetKeyStringToCharacter[key];
-        return (char)0; // No key found
+            character = C64AspNetKeyboard.AspNetKeyStringToCharacter[key];
+        return character;
+    }
+
+    private void CaptureJoystick(C64 c64)
+    {
+        var joystick = c64.Cia.Joystick;
+
+        // Use keypresses as joystick input for now.
+        if (joystick.KeyboardJoystickEnabled)
+        {
+            var joystick1KeyboardMap = joystick.KeyboardJoystickMap.KeyToJoystick1Map;
+            var joystick1Actions = new HashSet<C64JoystickAction>();
+            foreach (var charCode in joystick1KeyboardMap.Keys)
+            {
+                string key = charCode.ToString().ToLower();
+                if (_inputHandlerContext!.KeysDown.Contains(key))
+                    joystick1Actions.Add(joystick1KeyboardMap[charCode]);
+            }
+            c64.Cia.Joystick.SetJoystick1Actions(joystick1Actions);
+
+            var joystick2KeyboardMap = joystick.KeyboardJoystickMap.KeyToJoystick2Map;
+            var joystick2Actions = new HashSet<C64JoystickAction>();
+            foreach (var charCode in joystick2KeyboardMap.Keys)
+            {
+                string key = charCode.ToString().ToLower();
+                if (_inputHandlerContext!.KeysDown.Contains(key))
+                    joystick2Actions.Add(joystick2KeyboardMap[charCode]);
+            }
+            c64.Cia.Joystick.SetJoystick2Actions(joystick2Actions);
+        }
     }
 
     public List<string> GetStats()
