b1ec78b4a2cd013dfe23d3bb8f172e25843dd276
Highbyte <highbyte@outlook.com>
2024-02-12 22:05:44 +0100

    Add more benchmarks

--- benchmarks/Highbyte.DotNet6502.Benchmarks/ExecuteInstruction.cs
+++ benchmarks/Highbyte.DotNet6502.Benchmarks/ExecuteInstruction.cs
@@ -6,15 +6,15 @@ using BenchmarkDotNet.Jobs;
 namespace Highbyte.DotNet6502.Benchmarks;
 
 [MemoryDiagnoser] // Memory diagnoser is used to measure memory allocations
-[ShortRunJob] // WARNING: ShortRunJob is a custom job runs faster than normal, but is less accurate.
+//[ShortRunJob] // WARNING: ShortRunJob is a custom job runs faster than normal, but is less accurate.
 //[DryJob] // DANGER: DryJob is a custom job that runs very quickly, but VERY INACCURATE. Use only to verify that benchmarks actual runs.
-public class ExecuteInstructions
+public class ExecuteInstruction
 {
     private CPU _cpu = default!;
     private Memory _mem = default!;
     private ushort _startAddress;
 
-    [Params(5)]
+    [Params(1)]
     //[Params(1, 10, 100)]
     public int NumberOfInstructionsToExecute;
 
@@ -26,30 +26,39 @@ public class ExecuteInstructions
 
         _cpu = new CPU();
         _mem = new Memory();
-        _startAddress = 0x0100;
+        _startAddress = 0xc000;
+        LoadProgram(_mem, _startAddress);
 
-        // Random code (could include illegal opcodes)
-        //var bytes = new byte[64 * 1024];
-        //new Random(42).NextBytes(bytes);
-        //_mem.StoreData(0, bytes);
+    }
 
+    private void LoadProgram(Memory mem, ushort startAddress)
+    {
+        var bytes = new byte[64 * 1024];
+        //// Random code (could include illegal opcodes)
+        ////new Random(42).NextBytes(bytes);
+        //for (int i = 0; i < bytes.Length; i++)
+        //{
+        //    bytes[i] = (byte)OpCodeId.NOP;
+        //}
+        //mem.StoreData(0, bytes);
+
+        //JSR + LDA_I + RTS + LDA_I + JMP-> 5 instructions
         // Code at start address
         ushort branchAddress = (ushort)(_startAddress + 0x10);
         var address = _startAddress;
-        _mem.WriteByte(ref address, OpCodeId.JSR);
-        _mem.WriteWord(ref address, branchAddress);
-        _mem.WriteByte(ref address, OpCodeId.LDA_I);
-        _mem.WriteByte(ref address, 42);
-        _mem.WriteByte(ref address, OpCodeId.JMP_ABS);
-        _mem.WriteWord(ref address, _startAddress); // Jump back to start address
+        mem.WriteByte(ref address, OpCodeId.JSR);
+        mem.WriteWord(ref address, branchAddress);
+        mem.WriteByte(ref address, OpCodeId.LDA_I);
+        mem.WriteByte(ref address, 42);
+        mem.WriteByte(ref address, OpCodeId.JMP_ABS);
+        mem.WriteWord(ref address, _startAddress); // Jump back to start address
 
         // Code at branch jsr address
         address = branchAddress;
-        _mem.WriteByte(ref address, OpCodeId.LDA_I);
-        _mem.WriteByte(ref address, 21);
-        _mem.WriteByte(ref address, OpCodeId.RTS);
+        mem.WriteByte(ref address, OpCodeId.LDA_I);
+        mem.WriteByte(ref address, 21);
+        mem.WriteByte(ref address, OpCodeId.RTS);
 
-        // JSR + LDA_I + RTS + LDA_I + JMP -> 5 instructions
     }
 
     //[GlobalCleanup]
--- benchmarks/Highbyte.DotNet6502.Benchmarks/Program.cs
+++ benchmarks/Highbyte.DotNet6502.Benchmarks/Program.cs
@@ -3,10 +3,22 @@ using BenchmarkDotNet.Running;
 using Highbyte.DotNet6502.Benchmarks;
 
 // If debugging, uncomment line below
-//BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args, new DebugInProcessConfig());
+//BenchmarkSwitcher
+//    .FromAssembly(typeof(Program).Assembly)
+//    .Run(args, new DebugInProcessConfig());
 
 // Or if just wanting a UI to select which benchmark to run, uncomment line below
-//BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+//BenchmarkSwitcher
+//    .FromAssembly(typeof(Program).Assembly)
+//    .Run(args);
 
-// Run benchmarks
-var summary = BenchmarkRunner.Run<ExecuteInstructions>();
+// Or run specific benchmarks
+
+//var summary = BenchmarkRunner
+//    .Run<ExecuteInstruction>();
+
+//var summary = BenchmarkRunner
+//    .Run<ExecuteAllInstructions>();
+
+var summary = BenchmarkRunner
+    .Run<C64ExecuteInstruction>();
