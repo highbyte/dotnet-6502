4b7efd1b1c061a305f347c989df8f0d1b5c55d8b
Highbyte <highbyte@outlook.com>
2024-07-18 22:28:14 +0200

    Fix WASM C64 joystick keyboard

--- src/libraries/Highbyte.DotNet6502.Impl.AspNet/Commodore64/Input/C64AspNetInputHandler.cs
+++ src/libraries/Highbyte.DotNet6502.Impl.AspNet/Commodore64/Input/C64AspNetInputHandler.cs
@@ -107,7 +107,7 @@ public class C64AspNetInputHandler : IInputHandler
         // Note: Assume Keyboard input has been processed before this, so that Joystick actions based on keypresses has resulted 
         //       in the current joystick actions being initialized this frame (and may contain actions from keyboard).
         //       Thus "overwrite" is set to false so that keyboard actions are not overwritten.
-        c64.Cia.Joystick.SetJoystickActions(_c64AspNetConfig.CurrentJoystick, c64JoystickActions);
+        c64.Cia.Joystick.SetJoystickActions(_c64AspNetConfig.CurrentJoystick, c64JoystickActions, overwrite: false);
     }
 
     private HashSet<C64JoystickAction> GetC64JoystickActionsFromAspNetGamepad(HashSet<int> gamepadButtonsDown)
