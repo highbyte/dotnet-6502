a679309356be49fe70a8838fbb672ac5defb326f
Highbyte <highbyte@outlook.com>
2025-10-29 15:24:30 +0100

    Comment

--- src/apps/Avalonia/Highbyte.DotNet6502.App.Avalonia.Core/SystemSetup/HostConfigJsonContext.cs
+++ src/apps/Avalonia/Highbyte.DotNet6502.App.Avalonia.Core/SystemSetup/HostConfigJsonContext.cs
@@ -2,6 +2,10 @@ using System.Text.Json.Serialization;
 
 namespace Highbyte.DotNet6502.App.Avalonia.Core.SystemSetup;
 
+/// <summary>
+/// Source generation context for host config JSON serialization.
+/// Makes it AOT friendly and faster.
+/// </summary>
 [JsonSourceGenerationOptions(
     PropertyNameCaseInsensitive = true // your option from the code
 )]
