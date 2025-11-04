f778ad791807f76a4e1f365f0ca606781d8decef
Highbyte <highbyte@outlook.com>
2025-11-04 20:24:29 +0100

    Titles and favicon

--- src/apps/Avalonia/Highbyte.DotNet6502.App.Avalonia.Core/Views/MainWindow.axaml.cs
+++ src/apps/Avalonia/Highbyte.DotNet6502.App.Avalonia.Core/Views/MainWindow.axaml.cs
@@ -13,7 +13,6 @@ public partial class MainWindow : Window
         InitializeComponent();
 #endif
 
-        Title = "Highbyte.DotNet6502 Emulator - Avalonia";
         // Remove fixed Width since SizeToContent="Width" will handle it
         Height = 800;
         MinWidth = 650; // Minimum to accommodate first two columns (250 + 400)
