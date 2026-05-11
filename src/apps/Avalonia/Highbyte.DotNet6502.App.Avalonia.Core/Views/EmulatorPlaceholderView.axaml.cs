244bff35bc7071aebcf49c746f0487efa2d44895
Highbyte <highbyte@outlook.com>
2026-05-11 19:54:21 +0200

    Add links to start screen

--- src/apps/Avalonia/Highbyte.DotNet6502.App.Avalonia.Core/Views/EmulatorPlaceholderView.axaml.cs
+++ src/apps/Avalonia/Highbyte.DotNet6502.App.Avalonia.Core/Views/EmulatorPlaceholderView.axaml.cs
@@ -1,6 +1,8 @@
 using System;
+using System.Threading.Tasks;
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Highbyte.DotNet6502.App.Avalonia.Core.ViewModels;
 
 namespace Highbyte.DotNet6502.App.Avalonia.Core.Views;
@@ -79,5 +81,21 @@ public partial class EmulatorPlaceholderView : UserControl
         var scaledWidth = _displayWidth * Scale;
         var scaledHeight = _displayHeight * Scale;
         return new Size(scaledWidth, scaledHeight);
+    }
+
+    private void OpenGitHub_Click(object? sender, RoutedEventArgs e)
+        => SafeAsyncHelper.Execute(() => LaunchUriIfAvailableAsync("https://github.com/highbyte/dotnet-6502"));
+
+    private void OpenHomePage_Click(object? sender, RoutedEventArgs e)
+        => SafeAsyncHelper.Execute(() => LaunchUriIfAvailableAsync("https://highbyte.se"));
+
+    private Task LaunchUriIfAvailableAsync(string uri)
+    {
+        if (TopLevel.GetTopLevel(this) is { } topLevel)
+        {
+            return topLevel.Launcher.LaunchUriAsync(new Uri(uri));
+        }
+
+        return Task.CompletedTask;
     }
 }
